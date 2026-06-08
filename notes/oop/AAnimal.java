8cb878f3d42b5e9d077fe62d66d54a3fe146b437
Deepak Kumar <deepak.kumar@suhora.com>
2026-06-08 17:38:00 +0530

    rev

---
+++ notes/oop/AAnimal.java
@@ -0,0 +1,9 @@
+
+
+abstract  class AAnimal{
+    public AAnimal(){
+        
+    }
+     abstract  public void sound();
+    abstract public void name();
+}
---
+++ notes/oop/IAnimal.java
@@ -0,0 +1,10 @@
+
+interface Animal {
+
+    public void sound();
+
+    public void name();
+
+    abstract void pMethod();
+
+}
